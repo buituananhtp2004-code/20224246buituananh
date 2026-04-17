b246f6d0f6fc4be1e6f1a17d9a8f458e31ab53ac
buituananhtp2004-code <buituananhtp2004@gmail.com>
2026-04-17 11:41:52 +0700

    Add files via upload

---
+++ storage/framework/views/e7296f2d7fa2a657bb4f115c4bc9fc67.php
@@ -0,0 +1,26 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="card">
+        <h2>Thêm sinh viên</h2>
+
+        <?php if($errors->any()): ?>
+            <div class="alert alert-error">
+                <ul style="margin: 0; padding-left: 18px;">
+                    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <li><?php echo e($error); ?></li>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+
+        <form action="<?php echo e(route('students.store')); ?>" method="POST">
+            <?php echo csrf_field(); ?>
+            <?php echo $__env->make('students._form', ['student' => null], array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+            <input type="submit" value="Lưu" class="button">
+            <a class="button button-secondary" href="<?php echo e(route('students.index')); ?>">Quay lại</a>
+        </form>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\quanlysinhvien\resources\views/students/create.blade.php ENDPATH**/ ?>
